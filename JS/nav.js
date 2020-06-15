a430ae46e99a330d67bfa84a96960d1ee6fd940a
Littl3S3al <lauseal@gmail.com>
2020-06-15 13:13:25 +0200

    changes by laura

--- JS/nav.js
+++
@@ -1,5 +0,0 @@
-const vala = document.querySelector('.close')
-
- vala.addEventListener('click', e =>{
-    location.href = "index.html";
-  })
--- JS/scripts.js
+++ JS/scripts.js
@@ -1,23 +1,15 @@
-//styling the burger menu
-function myFunction(x) {
-    x.classList.toggle("change");
-  }
+const btn = document.querySelector('.icon-menu');
+const menu = document.querySelector('.mobile-menu');
+let open = false;
 
-  const menu = document.querySelector('#navbar');
-  const burger = document.querySelector('.container');
-  
-
-  burger.addEventListener('click', e =>{
-      //menu.style.display = "block";
-      location.href = "navbar.html";
-  })
-
- /* Open navbar overlay on mobile*/
-function openNav() {
-    document.getElementById("myNav").style.display = "block";
-    console.log('hello');
+btn.addEventListener('click', () => {
+  if(!open){
+    menu.style.left = 0;
+    btn.innerHTML = '<i class="fas fa-times"></i>';
+    open = true;
+  } else {
+    menu.style.left = '100vh'
+    btn.innerHTML = '<i class="fas fa-bars"></i>';
+    open = false;
   }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.display = "none";
-  }
+})
